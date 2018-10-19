8289983a2203798aa1668b7f08152f0a150291f6
Fajar Febbi Aldianto <43521209+fajarfaldianto18@users.noreply.github.com>
2018-10-19 10:07:34 +0700

    jurnal 7
    fajar febbi aldianto
    6701174130
    d3mi 41 04

---
+++ belajar.php
@@ -0,0 +1,16 @@
+<?php
+	$server ="localhost";
+	$username ="root";
+	$password="";
+	$db ="datamhs";
+
+	$koneksi =mysqli_connect($server, $username, $password, $db);
+
+		if($koneksi){
+			echo "koneksi sukses";
+		}
+
+		else{
+			echo "Koneksi gagal";
+		}
+ ?>
